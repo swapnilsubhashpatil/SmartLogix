6184263d36645648a36179b884f63385320bc957
swapnilsubhashpatil <patilswapnilsubhash@gmail.com>
2025-03-30 22:25:44 +0530

    removed server.js

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: `http://localhost:${PORT}/auth/google/callback`,
+      callbackURL: `https://smartlogix.onrender.com/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
